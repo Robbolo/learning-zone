b6b30b6a6efafeedfcd6ed112849442dd2d0941a
Robbolo <167103133+Robbolo@users.noreply.github.com>
2025-10-31 12:36:01 +0000

    write test rows to db

--- sqlalchemy/db_writer.py
+++ sqlalchemy/db_writer.py
@@ -1,1 +1,16 @@
 from sqlalchemy.orm import sessionmaker
+from connect import engine
+from model import Person
+
+# setup session for connection
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# make rows via model class
+
+person1 = Person(name="Rob", age=37)
+person2 = Person(name="Sarah", age=38)
+
+# add and commit rows to db
+session.add_all([person1, person2])
+session.commit()
