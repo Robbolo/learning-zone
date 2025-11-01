51895189f922eb4630670731902e652f3c3ad918
Robbolo <167103133+Robbolo@users.noreply.github.com>
2025-11-01 09:27:00 +0000

    added changelog to shellscript

--- liquibase/run_liquibase.sh
+++ liquibase/run_liquibase.sh
@@ -2,4 +2,5 @@
 docker run --rm \
   -v "$(pwd)":/liquibase \
   liquibase/liquibase \
-  --defaultsFile=liquibase.properties update
+  --defaultsFile=liquibase.properties update \
+  --changeLogFile=/liquibase/master-changelog.xml \
