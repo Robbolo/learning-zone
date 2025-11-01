497df39245c54fa1a75ebd7cae53b3728698e04f
Robbolo <167103133+Robbolo@users.noreply.github.com>
2025-11-01 20:22:56 +0000

    removed version number

--- liquibase_dev/run_liquibase.ps1
+++
@@ -1,4 +0,0 @@
-docker run --rm -v "D:\Coding\Repos\learning-zone\liquibase_dev:/liquibase-host" `
-  liquibase/liquibase `
-  --changeLogFile=/liquibase-host/master-changelog.xml `
-  --defaultsFile=/liquibase-host/liquibase.properties update
