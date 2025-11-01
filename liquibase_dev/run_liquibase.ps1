9a27703860d9db391695bdf30585c42e20ec0821
Robbolo <167103133+Robbolo@users.noreply.github.com>
2025-11-01 11:41:50 +0000

    changed bashscript to powershell

--- liquibase_dev/run_liquibase.ps1
+++ liquibase_dev/run_liquibase.ps1
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm \
-  -v "D:/Coding/Repos/learning-zone/liquibase_dev:/liquibase-host" \
+  -v "D:\Coding\Repos\learning-zone\liquibase_dev:/liquibase-host" \
   liquibase/liquibase \
   --changeLogFile=/liquibase-host/master-changelog.xml \
   --defaultsFile=/liquibase-host/liquibase.properties update 
